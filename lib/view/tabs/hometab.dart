e61b0f27e8cd3708a999b5a854ceccc292439d6a
ANUSHKA DAS <80618826+Soumi-igtm@users.noreply.github.com>
2022-06-18 17:48:32 +0530

    new commit

--- lib/view/tabs/hometab.dart
+++ lib/view/tabs/hometab.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import '../../ui/common.dart';
+import '../../ui/custom_colors.dart';
 import '../../ui/rails.dart';
+import '../../ui/remixicon.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
@@ -13,22 +16,51 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(20),
-      shrinkWrap: true,
-      children: [
-        Rails.instance.buildMyFavs(context),
-        const SizedBox(height: 15),
-        headingText(
-          text: "This is where the musical journey starts",
-          fontSize: 30,
-          fontStyle: FontStyle.italic,
-          textAlign: TextAlign.center,
-          fontWeight: FontWeight.bold,
-        ),
-        customDivider(),
-        Rails.instance.buildRecentRecordings(context),
-      ],
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: ListView(
+          padding: const EdgeInsets.all(20),
+          shrinkWrap: true,
+          children: [
+            Rails.instance.buildMyFavs(context),
+            const SizedBox(height: 15),
+            headingText(
+              text: "This is where the musical journey starts",
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+              textAlign: TextAlign.center,
+              fontWeight: FontWeight.bold,
+            ),
+            customDivider(),
+            Rails.instance.buildRecentRecordings(context),
+          ],
+      ),
+      floatingActionButton:  SpeedDial(
+        animatedIcon: AnimatedIcons.list_view,
+        backgroundColor: Colors.white,
+        children: [
+          SpeedDialChild(
+            child: Icon(Remix.upload_fill),
+            label: 'Upload',
+            backgroundColor: Colors.white,
+          ),
+          SpeedDialChild(
+              child: Icon(Remix.upload_fill),
+              label: 'Add artist',
+            backgroundColor: Colors.white,
+          ),
+          SpeedDialChild(
+              child: Icon(Remix.upload_fill),
+              label: 'Withdraw',
+            backgroundColor: Colors.white,
+          ),
+          SpeedDialChild(
+              child: Icon(Remix.upload_fill),
+              label: 'New release',
+              backgroundColor: Colors.white,
+          ),
+        ],
+      ),
     );
   }
 }
